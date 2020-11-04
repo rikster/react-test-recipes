5044e2a18587c5b8c118840dd4fe47f1de886c74
Richard Hounslow <rhounslow@gmail.com>
2020-11-04 19:19:43 +1100

    CRA cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './components/hello'
+import Hello from "./components/hello";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Hello/>
+    );
 }
 
 export default App;
