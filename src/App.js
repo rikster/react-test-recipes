832acdcbc3100332678443f0acb44cecb9c7b793
Richard Hounslow <rhounslow@gmail.com>
2020-11-05 12:00:47 +1100

    Toggle without test (added onchange handler)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './components/hello'
 import Hello from "./components/hello";
+import Toggle from "./components/toggle";
 
 function App() {
+
+    const onChange = (state) => {
+        console.log(state);
+    }
+
     return (
-        <Hello/>
+        <Toggle onChange={onChange}/>
     );
 }
 
